f9eb4fba8ea7807100fed13807d6bf35d2d5c48a
suvamligal <suvamligal@gmail.com>
2024-04-23 15:08:47 -0500

    welcome commit

---
+++ src/main/java/org/lashoo/lyakheyv1/LyakheyV1Application.java
@@ -0,0 +1,13 @@
+package org.lashoo.lyakheyv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LyakheyV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LyakheyV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/lashoo/lyakheyv1/LyakheyV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lashoo.lyakheyv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LyakheyV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
